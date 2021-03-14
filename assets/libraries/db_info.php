0b7e331270d567410316e562c4fc61bbd2cb0dcf
rutherlesdev <rutherlesdev@gmail.com>
2021-03-14 15:10:00 -0300

    teste1

--- assets/libraries/db_info.php
+++ assets/libraries/db_info.php
@@ -2,7 +2,7 @@
 
 date_default_timezone_set('Asia/Kuala_Lumpur');
 define('TSITE_SERVER','localhost');
-define('TSITE_DB','kgwywvbzdz');
-define('TSITE_USERNAME','kgwywvbzdz');
-define('TSITE_PASS','8WSFDByH9P');
+define('TSITE_DB','fhngjkvcbn');
+define('TSITE_USERNAME','fhngjkvcbn');
+define('TSITE_PASS','EVTJK7udet');
 ?>
